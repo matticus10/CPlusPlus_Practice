39ee0fefd3eba27786408bc2e9b32cf5e3f0a0fc
Crasicus <matticus10@hotmail.com>
2017-06-21 12:56:29 +0100

    Completed Lecture 21

--- CPlusPlus_Udemy/Section_02/BullCowGame/main.cpp
+++ CPlusPlus_Udemy/Section_02/BullCowGame/main.cpp
@@ -3,22 +3,38 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+// the entry point for our application
 int main()
+{
+	PrintIntro();
+	
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;
+}
+
+void PrintIntro()
 {
 	// introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
 	cout << endl;
+	return;
+}
 
+string GetGuessAndPrintBack()
+{
 	// get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	// repeat the guess back to them
+
+	// print the guess back to them
 	cout << "Your guess was: " << Guess << endl;
-
-
-	cout << endl;
-	return 0;
+	return Guess;
 }
