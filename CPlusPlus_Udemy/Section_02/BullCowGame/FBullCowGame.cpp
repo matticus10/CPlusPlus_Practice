14f5b7d514ff91f619284cf293935e0c713066ab
Matt Rogers <matticus10@hotmail.com>
2017-07-08 09:54:50 +0100

    Minor Fixes

--- CPlusPlus_Udemy/Section_02/BullCowGame/FBullCowGame.cpp
+++ CPlusPlus_Udemy/Section_02/BullCowGame/FBullCowGame.cpp
@@ -21,7 +21,7 @@ int32 FBullCowGame::GetMaxTries() const
 
 void FBullCowGame::Reset()
 {
-	const FString HIDDEN_WORD = "planet"; // this MUST be an isogram
+	const FString HIDDEN_WORD = "posted"; // this MUST be an isogram
 	MyHiddenWord = HIDDEN_WORD;
 	MyCurrentTry = 1;
 	bGameIsWon = false;
